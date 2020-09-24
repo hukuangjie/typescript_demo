c38ddada4df9d660394f8e9649ab67bd81a5dba6
胡旷杰 <754432607@qq.com>
2020-09-24 10:57:40 +0800

    interface 接口 学习

---
+++ demo7.ts
@@ -0,0 +1,22 @@
+// /*
+//  * @Author: your name
+//  * @Date: 2020-09-24 09:31:32
+//  * @LastEditTime: 2020-09-24 10:31:35
+//  * @LastEditors: Please set LastEditors
+//  * @Description: In User Settings Edit
+//  * @FilePath: \typescript_demo\demo7.ts
+//  */
+
+// // const xiaojiejie: (string | number)[] = ['dajiao', 'teacher', 28]
+
+// const xiaojiejie: [string, string, number] = ['dajiao', 'teacher', 28]
+// //CSV
+
+
+// // 元祖
+// const xiaojiejies: [string, string, number][] = [
+//     ['dajiao', 'teacher', 18],
+//     ['dajiao', 'teacher', 18],
+//     ['dajiao', 'teacher', 18]
+// ]
+// // 现在一般使用 json 格式
---
+++ demo8.ts
@@ -0,0 +1,45 @@
+// /*
+//  * @Author: your name
+//  * @Date: 2020-09-24 10:32:02
+//  * @LastEditTime: 2020-09-24 10:44:25
+//  * @LastEditors: Please set LastEditors
+//  * @Description: In User Settings Edit
+//  * @FilePath: \typescript_demo\demo8.ts
+//  */
+
+// interface Girl {
+//     name: string;
+//     age: number;
+//     bust: number;
+//     waisline?: number
+// }
+
+// // type Girl1 = {
+// //     name: string,
+// //     age: number,
+// //     bust: number
+// // }
+
+
+
+// const girl = {
+//     name: '大脚',
+//     age: 18,
+//     bust: 94,
+//     waisline: 23
+// }
+
+// const screenResume = (girl: Girl) => {
+//     girl.age < 24 && girl.bust >= 90 && console.log(girl.name + '进入面试');
+//     girl.age >= 24 || girl.bust < 90 && console.log(girl.name + '你被淘汰');
+// }
+// const getResume = (girl: Girl) => {
+//     console.log(girl.name + '年龄是' + girl.age);
+//     console.log(girl.name + '胸围是' + girl.bust);
+//     girl.waisline && console.log(girl.name + '腰围是' + girl.waisline);
+
+// }
+
+// screenResume(girl)
+// getResume(girl)
+
